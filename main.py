3ceb875cb2dcf80cdfa831bf989c307c26790593
unknown <israel.igietsemhe@ketteq.com>
2024-03-15 00:01:47 -0400

    bind port to listen t external IPs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS
 from recommender import get_recommendations
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"*":{"origins":"*"}})
@@ -30,4 +31,5 @@ def reco_system():
 #         return render_template('questions.html', message='Please enter required fields')
     
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
