a99ced5872c4169f13f2c019b89b4b3a0c43550c
unknown <israel.igietsemhe@ketteq.com>
2024-03-15 00:26:02 -0400

    added other app routes

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request,jsonify
-#from flask_cors import CORS
 from recommender import get_recommendations
 import os
 
 app = Flask(__name__)
-#CORS(app, resources={r"*":{"origins":"*"}})
 
-# @app.route('/')
-# def main():
-#     return render_template('questions.html')
+@app.route('/')
+def main():
+    return render_template('questions.html')
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/questions', methods=['GET','POST'])
 def reco_system():
 
     if request.method == 'GET':
@@ -21,15 +19,14 @@ def reco_system():
         questions = get_recommendations(lessonId)
         return questions
 
-# @app.route('/submit', methods=['POST', 'GET'])
-# def submit():
-#     if request.method == 'POST' or request.method == 'GET':
-#         lessonId = request.form['lessonId']
-#         questions = get_recommendations(lessonId)
-#         return questions
-#     else:
-#         return render_template('questions.html', message='Please enter required fields')
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST' or request.method == 'GET':
+        lessonId = request.form['lessonId']
+        questions = get_recommendations(lessonId)
+        return questions
+    else:
+        return render_template('questions.html', message='Please enter required fields')
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
